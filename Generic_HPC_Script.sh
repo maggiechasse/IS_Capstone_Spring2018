64741498ed328432be6497799f2f9711043ad1c7
Maggie Chasse <31742947+maggiechasse@users.noreply.github.com>
2018-05-18 15:39:20 -0500

    Create Generic_HPC_Script.sh

---
+++ Generic_HPC_Script.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+#PBS -l walltime=200:00:00 #Max run time is 200 hours
+#PBS -l mem=20gb #Request 20gb memory
+#PBS -l nodes=1:ppn=1 #1 node
+#PBS -M maggie.chasse@vai.org #Email
+#PBS -m abe #Abort; Beginning; End emails
+#PBS -N name #Name of the job
+cd $PBS_O_WORKDIR #Change to working directory
